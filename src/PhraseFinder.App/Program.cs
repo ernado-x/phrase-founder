52c9134f559bc1de7fb68c0e608a471843e84d1b
Andrew <andrew.gubskiy@outlook.com>
2022-12-27 15:56:06 +0200

    first implementation

--- src/PhraseFinder.App/Program.cs
+++ src/PhraseFinder.App/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using PhraseFinder.Core;
+
 Console.WriteLine("Hello, World!");
+
+var path = "/Users/andrew/Projects/phrase-founder/data/gec-fluency/train/source";
+var files = Directory.GetFiles(path).Take(50).ToList();
+
+var pi = "3.14159265359";
+
+foreach (var file in files)
+{
+    var text = await File.ReadAllTextAsync(file);
+    var results = Finder.find(text, pi);
+
+    foreach (var result in results)
+    {
+        Console.WriteLine($"{result}");
+    }
+}
